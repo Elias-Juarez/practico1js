b207d9deb0247252f27e2ea2636b74b7756eb487
Elias Juarez <juarezelias83@gmail.com>
2023-10-28 15:33:52 -0300

    se realizo el ejercicio 5 del practico 1 de js

---
+++ js/practicoEjercicio5.js
@@ -0,0 +1,5 @@
+// Ejercicio 5
+
+let numero1 = parseInt(prompt("por favor ingrese un número"));
+let numero2 = parseInt(prompt("por favor ingrese otro número"));
+document.write(numero1 + numero2);
